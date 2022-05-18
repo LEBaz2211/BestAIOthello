86c46bc66dd4698d25b8ad9196e3c1c77808f863
LEBaz2211 <92997786+LEBaz2211@users.noreply.github.com>
2022-05-18 15:06:21 +0200

    Update game_runner.py

--- game_runner.py
+++ game_runner.py
@@ -4,7 +4,7 @@ import atexit
 #              subscription address      game address
 #                       |                     |
 #                       v                     v
-player1 = Player(("127.0.0.1", 3000), ("127.0.0.1", 8080), "The_potential_better", ["47369", "25314"])
+player1 = Player(("127.0.0.1", 3000), ("127.0.0.1", 8080), "The_potential_better", ["20269", "18402"])
 
 player1.sub()
 t = threading.Thread(target = player1.run)
